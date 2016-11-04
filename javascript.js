5103d9179d2e7b960a45cfc9fc1881395de40f8c
HerrR <rbergel@kth.se>
2016-11-04 09:14:16 +0100

    Sista dagen DN Hack

---
+++ javascript.js
@@ -0,0 +1,26 @@
+function sayHello(){
+	// Kod för att säga hej
+}
+
+function pausePlay(){
+	// Kod för att pausa / playa animationen
+}
+
+function vadArKlockan(){
+	// Kod för att visa vad klockan är
+}
+
+function uppdateraKlockan(){
+	// Kod för att hämta ny tid
+}
+
+function svara(){
+	// Kod som körs när man svarar på frågan
+}
+
+function showHideMenu(){
+	// Kod för att vias / dölja menyn
+}
+
+
+
